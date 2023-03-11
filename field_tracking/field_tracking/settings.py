a183b8264642a64fc56d3e5d3c8ee0908a4b6450
ruhidibadli <ruhid.ibadli@gmail.com>
2023-03-11 02:02:16 +0000

    port data changed

--- field_tracking/field_tracking/settings.py
+++ field_tracking/field_tracking/settings.py
@@ -87,11 +87,10 @@ print(env("DB_ENGINE"))
 DATABASES = {
     'default': {
         'ENGINE': env("DB_ENGINE"),
-        'NAME': env("DB_NAME"), 
+        'NAME': env("DB_NAME"),
         'USER': env("DB_USER"),
         'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("DB_HOST"),
-        'PORT': env("DB_PORT")
+        'HOST': env("DB_HOST")
     }
 }
 
