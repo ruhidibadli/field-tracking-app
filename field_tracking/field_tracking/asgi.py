f7929708a52f4e15dfe9c355a68145420a3931dc
ruhidibadli <ruhid.ibadli@gmail.com>
2023-03-11 17:34:10 +0300

    Revert "Websocket Added."
    This reverts commit 9eeb4885e896cac1b8136cffc54d42649fabfdf0.

--- field_tracking/field_tracking/asgi.py
+++ field_tracking/field_tracking/asgi.py
@@ -10,17 +10,7 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import accounts.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'field_tracking.settings')
 
-application = ProtocolTypeRouter({
-    'http':get_asgi_application(),
-    'websocket':AuthMiddlewareStack(
-        URLRouter(
-            accounts.routing.websocket_urlpatterns
-        )
-    )
-})
+application = get_asgi_application()
